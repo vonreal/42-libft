56488337440762b6f1a9717fb8a4c16f5a451c9c
Jiun Na <vonreal20@gmail.com>
2021-07-21 05:47:52 +0000

    ft_strlcpy.c fixed

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jna <jna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 16:26:40 by jna               #+#    #+#             */
-/*   Updated: 2020/11/21 04:10:42 by jna              ###   ########.fr       */
+/*   Updated: 2021/07/21 05:47:30 by jna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	unsigned int	count;
 
 	count = 0;
-	if (dst == '\0' && src == '\0')
+	if (dst == NULL && src == NULL)
 		return (0);
 	if (dstsize == 0)
 		return (ft_strlen(src));
